294fd1473e7d8c7313722ae8f1dc719619bb8a6c
Jules Emond <julesemond@outlook.com>
2021-04-13 10:51:26 -0400

    Ajout du mouvement de camera basique + animations joueur

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -11,25 +11,33 @@ public class PlayerScript : MonoBehaviour
     private float speed = 2.5f;
     private float jumpHeight = 5f;
     private SpriteRenderer _renderer;
+    private Rigidbody2D _rb;
+    [SerializeField] private Camera cam;
+    
+    private bool _touchingGround = true;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         _renderer = GetComponent<SpriteRenderer>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Movements();
+        Animate();
     }
 
     void Movements()
     {
-        Vector2 pos = transform.position;
-        if (Input.GetKey ("w")) {
-            pos.y += speed * Time.deltaTime * 2;
+        Vector3 pos = new Vector3();
+        
+        if (Input.GetKey ("w") && _touchingGround) {
+            _rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+            _touchingGround = false;
         }
         if (Input.GetKey ("d")) {
             pos.x += speed * Time.deltaTime;
@@ -40,7 +48,26 @@ public class PlayerScript : MonoBehaviour
             _renderer.flipX = true;
         }
         transform.rotation = Quaternion.Euler(0,0,0);
-        transform.position = pos;
+        transform.position = pos + transform.position;
+        cam.transform.position = new Vector3(pos.x + cam.transform.position.x, cam.transform.position.y, cam.transform.position.z);
     }
-
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name != "")
+        {
+            _touchingGround = true;
+        }
+    }
+    
+    void Animate()
+    {
+        if (Input.GetKey("d") || Input.GetKey("a"))
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+    }
 }
