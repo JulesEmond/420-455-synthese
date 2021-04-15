8d39c53cba99325f090798287a2838acd0dab6ad
Antoine <antoine3wls@gmail.com>
2021-04-15 17:26:05 -0400

    Ajout de la fonction respawn

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,23 +1,20 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditorInternal;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
     public Animator anim;
-    private float speed = 2.5f;
-    private float jumpHeight = 5f;
-    private float diffX = 0f;
+    private const float Speed = 2.5f;
+    private const float JumpHeight = 5f;
     private SpriteRenderer _renderer;
     private Rigidbody2D _rb;
-    
+    private Vector3 _initialPosition;
     private bool _touchingGround = true;
+    private static readonly int IsWalking = Animator.StringToHash("isWalking");
 
     // Start is called before the first frame update
     void Start()
     {
+        _initialPosition = transform.position;
         anim = GetComponent<Animator>();
         _renderer = GetComponent<SpriteRenderer>();
         _rb = GetComponent<Rigidbody2D>();
@@ -33,23 +30,22 @@ public class PlayerScript : MonoBehaviour
     void Movements()
     {
         Vector3 pos = new Vector3();
-        float initX = transform.position.x;
         if (Input.GetKey ("w") && _touchingGround) {
-            _rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+            _rb.AddForce(Vector2.up * JumpHeight, ForceMode2D.Impulse);
             _touchingGround = false;
         }
         if (Input.GetKey ("d")) {
-            pos.x += speed * Time.deltaTime;
+            pos.x += Speed * Time.deltaTime;
             _renderer.flipX = false;
         }
         if (Input.GetKey ("a")) {
-            pos.x -= speed * Time.deltaTime;
+            pos.x -= Speed * Time.deltaTime;
             _renderer.flipX = true;
         }
-        transform.rotation = Quaternion.Euler(0,0,0);
-        transform.position = pos + transform.position;
-        float finalX = transform.position.x;
-        diffX = finalX - initX;
+
+        Transform transform1;
+        (transform1 = transform).rotation = Quaternion.Euler(0,0,0);
+        transform1.position = pos + transform1.position;
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
@@ -57,22 +53,22 @@ public class PlayerScript : MonoBehaviour
         {
             _touchingGround = true;
         }
+
+        if (other.gameObject.CompareTag("void"))
+        {
+            transform.position = _initialPosition;
+        }
     }
     
     void Animate()
     {
         if (Input.GetKey("d") || Input.GetKey("a"))
         {
-            anim.SetBool("isWalking", true);
+            anim.SetBool(IsWalking, true);
         }
         else
         {
-            anim.SetBool("isWalking", false);
+            anim.SetBool(IsWalking, false);
         }
-    }
-
-    public float GetDiffX()
-    {
-        return diffX;
     }
 }
