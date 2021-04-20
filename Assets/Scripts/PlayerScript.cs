f172c92319066c68b4fcae88c82f4acd8af37862
Antoine <antoine3wls@gmail.com>
2021-04-20 09:25:28 -0400

    Ajout des balles et du tire

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,23 +1,32 @@
-﻿using UnityEngine;
+﻿using System.Threading;
+using TMPro;
+using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
     public Animator anim;
     private const float Speed = 2.5f;
     private const float JumpHeight = 5f;
-    private SpriteRenderer _renderer;
+    private SpriteRenderer _playerRenderer;
     private Rigidbody2D _rb;
     private Vector3 _initialPosition;
     private bool _touchingGround = true;
     private static readonly int IsWalking = Animator.StringToHash("isWalking");
+
+    private GameObject weapon;
+
+    private SpriteRenderer _weaponRenderer;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         _initialPosition = transform.position;
         anim = GetComponent<Animator>();
-        _renderer = GetComponent<SpriteRenderer>();
+        _playerRenderer = GetComponent<SpriteRenderer>();
         _rb = GetComponent<Rigidbody2D>();
+        weapon = transform.GetChild(0).gameObject;
+        _weaponRenderer = weapon.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -36,11 +45,15 @@ public class PlayerScript : MonoBehaviour
         }
         if (Input.GetKey ("d")) {
             pos.x += Speed * Time.deltaTime;
-            _renderer.flipX = false;
+            _playerRenderer.flipX = false;
+            _weaponRenderer.flipX = false;
+            weapon.transform.position = new Vector3(transform.position.x + 0.87f, transform.position.y + 0.61f, 0);
         }
         if (Input.GetKey ("a")) {
             pos.x -= Speed * Time.deltaTime;
-            _renderer.flipX = true;
+            _playerRenderer.flipX = true;
+            _weaponRenderer.flipX = true;
+            weapon.transform.position = new Vector3(transform.position.x - 0.87f, transform.position.y + 0.61f, 0);
         }
 
         Transform transform1;
---
+++ Assets/Scripts/RevolverScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class RevolverScript : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+
+    private SpriteRenderer _gunRenderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gunRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if(Input.GetKey("space"))
+        {
+            GameObject bullet;
+            bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            Rigidbody2D bulletBody = bullet.GetComponent<Rigidbody2D>();
+            
+            if (_gunRenderer.flipX)
+            {
+                bulletBody.velocity = transform.TransformDirection(Vector2.left * 100);
+            }
+            else
+            {
+                bulletBody.velocity = transform.TransformDirection(Vector2.right * 100);
+            }
+            Destroy(bullet, 2f);
+        }
+    }
+}
